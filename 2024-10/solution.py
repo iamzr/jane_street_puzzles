c80ca674faf49fe45fa2c88264f622049225052f
Bradley-March <101671720+Bradley-March@users.noreply.github.com>
2024-10-08 22:10:13 +0100

    Fixed error with move list allowing incorrect moves

--- 2024-10/solution.py
+++ 2024-10/solution.py
@@ -30,7 +30,7 @@ class KnightsMoves:
         b[x + 2][y - 1]
         b[x + 2][y + 1]
         b[x - 1][y - 2]
-        b[x - 1][y + 1]
+        b[x - 1][y + 2]
         b[x + 1][y - 2]
         b[x + 1][y + 2]
 
@@ -44,7 +44,7 @@ class KnightsMoves:
                           (x + 2, y - 1),
                           (x + 2, y + 1),
                           (x - 1, y - 2),
-                          (x - 1, y + 1),
+                          (x - 1, y + 2),
                           (x + 1, y - 2),
                           (x + 1, y + 2)]
 
