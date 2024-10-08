2503c8a77b63b3b2c62d598e058a82d7246baae0
Bradley-March <101671720+Bradley-March@users.noreply.github.com>
2024-10-08 22:23:14 +0100

    Fixed orientation of board and path coordinates (bottom left of board not positioned properly)

--- 2024-10/set_abc_scoring.py
+++ 2024-10/set_abc_scoring.py
@@ -21,6 +21,8 @@ def get_board(a: int, b: int, c: int):
         [a, a, a, b, b, c],
         [a, a, a, b, b, c],
     ]
+
+    board = board[::-1]
     return board
 
 
@@ -38,16 +40,16 @@ def score_path(a: int, b: int, c: int, path: list[tuple[int, int]]) -> int:
     for new_move in path[1:]:
 
         # check if moving to the same letter and apply rules
-        if board[old_move[0]][old_move[1]] == board[new_move[0]][new_move[1]]:
-            score += board[new_move[0]][new_move[1]]
+        if board[old_move[1]][old_move[0]] == board[new_move[1]][new_move[0]]:
+            score += board[new_move[1]][new_move[0]]
         else:
-            score *= board[new_move[0]][new_move[1]]
+            score *= board[new_move[1]][new_move[0]]
 
         # same for flipped board
-        if board_flipped[old_move[0]][old_move[1]] == board_flipped[new_move[0]][new_move[1]]:
-            score_flipped += board_flipped[new_move[0]][new_move[1]]
+        if board_flipped[old_move[1]][old_move[0]] == board_flipped[new_move[1]][new_move[0]]:
+            score_flipped += board_flipped[new_move[1]][new_move[0]]
         else:
-            score_flipped *= board_flipped[new_move[0]][new_move[1]]
+            score_flipped *= board_flipped[new_move[1]][new_move[0]]
 
         # check if score is over 2024
         if (score > 2024) or (score_flipped > 2024):
@@ -57,11 +59,19 @@ def score_path(a: int, b: int, c: int, path: list[tuple[int, int]]) -> int:
 
     # check if score is corect at the end of the path
     if (score == 2024) and (score_flipped == 2024):
+        print(score, score_flipped)
         print(f"Path: {path}")
         print(f"a: {a}, b: {b}, c: {c}")
+        return
 
 
 # %%
-a, b, c, = 1, 2, 3
-for path in paths:
-    score_path(a, b, c, path)
+for a in range(1, 50):
+    for b in range(1, 50):
+        if a == b:
+            continue
+        for c in range(1, 50):
+            if c == a or c == b:
+                continue
+            for path in paths:
+                score_path(a, b, c, path)
